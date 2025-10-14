7af21addd3f1ba7a06de4f4c9ecd82d14a85134a
HAGELST0N <patrick.hagelston@gmail.com>
2025-10-14 13:31:59 -0400

    Apache Logs mostly complete except last item

---
+++ week4+5/ApacheLogs.ps1
@@ -0,0 +1,31 @@
+﻿Get-Content C:\xampp\apache\logs\access.log
+
+Get-Content C:\xampp\apache\logs\access.log -tail 5
+
+Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 ',' 400 '
+
+Get-Content C:\xampp\apache\logs\access.log | Select-String ' 200 ' -NotMatch
+
+$A = Get-ChildItem C:\xampp\apache\logs\*.log | Select-String -AllMatches 'error'
+$A[-5..-1]
+
+$notfounds = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
+
+
+$regex = [regex] "[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"
+
+$ipsUnorganized = $regex.Matches($notfounds)
+
+$ips = @()
+
+for($i=0; $i -lt $ipsUnorganized.Count; $i++)
+{
+    $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].value}
+}
+
+#$ips | Where-Object{$_.IP -ilike "10.*"}
+#
+$ipsoftens = $ips | Where-Object{$_.IP -ilike "10.*"}
+$counts = $ipsoftens | Group-Object IP
+$counts | Select-Object Count, Name
+#>
